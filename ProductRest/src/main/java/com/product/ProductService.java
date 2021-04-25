d70573d8a20c9b86e9201f4c22911f7151903031
sachin <sachin.kulkarni164@gmail.com>
2021-04-26 00:15:28 +0530

    Product RestApi Project

---
+++ ProductRest/src/main/java/com/product/ProductService.java
@@ -0,0 +1,30 @@
+package com.product;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll(){
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+		
+	}
+	
+	public Product get(Integer id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Integer id) {
+		repo.deleteById(id);
+	}
+}
