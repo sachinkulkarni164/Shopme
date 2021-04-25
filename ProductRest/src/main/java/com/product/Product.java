28334472e5ff3be3d4150924a4b2acb4affb862c
sachin <sachin.kulkarni164@gmail.com>
2021-04-26 01:31:46 +0530

    Changes to rest api program

--- ProductRest/src/main/java/com/product/Product.java
+++ ProductRest/src/main/java/com/product/Product.java
@@ -5,113 +5,95 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 public class Product {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int pId;
-	private String pName;
-	@Column(length = 3000)
-	private String pDesc;
-	private String pPhoto;
-	private int pPrice;
-	private int pDiscount;
-	private int pQuantity;
-	@ManyToOne
-	private Category category;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pId;
+    private String pName;
+    @Column(length = 3000)
+    private String pDesc;
+    private String pPhoto;
+    private int pPrice;
+    private int pDiscount;
+    private int pQuantity;
 
-	public Product() {
-	}
 
-	public Product(String pName, String pDesc, String pPhoto, int pPrice, int pDiscount, int pQuantity,
-			Category category) {
-		this.pName = pName;
-		this.pDesc = pDesc;
-		this.pPhoto = pPhoto;
-		this.pPrice = pPrice;
-		this.pDiscount = pDiscount;
-		this.pQuantity = pQuantity;
-		this.category = category;
-	}
+    public Product() {
+    }
 
-	public int getpId() {
-		return pId;
-	}
+    public Product(String pName, String pDesc, String pPhoto, int pPrice, int pDiscount, int pQuantity) {
+        this.pName = pName;
+        this.pDesc = pDesc;
+        this.pPhoto = pPhoto;
+        this.pPrice = pPrice;
+        this.pDiscount = pDiscount;
+        this.pQuantity = pQuantity;
+    }
 
-	public void setpId(int pId) {
-		this.pId = pId;
-	}
+    public int getpId() {
+        return pId;
+    }
 
-	public String getpName() {
-		return pName;
-	}
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
 
-	public void setpName(String pName) {
-		this.pName = pName;
-	}
+    public String getpName() {
+        return pName;
+    }
 
-	public String getpDesc() {
-		return pDesc;
-	}
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
 
-	public void setpDesc(String pDesc) {
-		this.pDesc = pDesc;
-	}
+    public String getpDesc() {
+        return pDesc;
+    }
 
-	public String getpPhoto() {
-		return pPhoto;
-	}
+    public void setpDesc(String pDesc) {
+        this.pDesc = pDesc;
+    }
 
-	public void setpPhoto(String pPhoto) {
-		this.pPhoto = pPhoto;
-	}
+    public String getpPhoto() {
+        return pPhoto;
+    }
 
-	public int getpPrice() {
-		return pPrice;
-	}
+    public void setpPhoto(String pPhoto) {
+        this.pPhoto = pPhoto;
+    }
 
-	public void setpPrice(int pPrice) {
-		this.pPrice = pPrice;
-	}
+    public int getpPrice() {
+        return pPrice;
+    }
 
-	public int getpDiscount() {
-		return pDiscount;
-	}
+    public void setpPrice(int pPrice) {
+        this.pPrice = pPrice;
+    }
 
-	public void setpDiscount(int pDiscount) {
-		this.pDiscount = pDiscount;
-	}
+    public int getpDiscount() {
+        return pDiscount;
+    }
 
-	public int getpQuantity() {
-		return pQuantity;
-	}
+    public void setpDiscount(int pDiscount) {
+        this.pDiscount = pDiscount;
+    }
 
-	public void setpQuantity(int pQuantity) {
-		this.pQuantity = pQuantity;
-	}
+    public int getpQuantity() {
+        return pQuantity;
+    }
 
-	public Category getCategory() {
-		return category;
-	}
+    public void setpQuantity(int pQuantity) {
+        this.pQuantity = pQuantity;
+    }
 
-	public void setCategory(Category category) {
-		this.category = category;
-	}
 
-	@Override
-	public String toString() {
-		return "Product{" + "pId=" + pId + ", pName=" + pName + ", pDesc=" + pDesc + ", pPhoto=" + pPhoto + ", pPrice="
-				+ pPrice + ", pDiscount=" + pDiscount + ", pQuantity=" + pQuantity + '}';
-	}
+    @Override
+    public String toString() {
+        return "Product{" + "pId=" + pId + ", pName=" + pName + ", pDesc=" + pDesc + ", pPhoto=" + pPhoto + ", pPrice=" + pPrice + ", pDiscount=" + pDiscount + ", pQuantity=" + pQuantity + '}';
+    }
 
-	// calculate price after discount
-	public int getPriceAfterApplyingDiscount() {
-		int d = (int) ((this.getpDiscount() / 100.0) * this.getpPrice());
-		return this.getpPrice() - d;
-	}
 
 }
